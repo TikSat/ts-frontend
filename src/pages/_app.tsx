be57ff5843e465502e956866b6283afe26a61575
Kirill Martynov <kirchik.martynov.995@gmail.com>
2022-07-13 13:19:05 +0300

    Feature, TS-48, Add aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/variables.css';
-import '../styles/globals.css';
+import 'src/styles/variables.css';
+import 'src/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
