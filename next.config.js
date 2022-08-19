04068a054aeaa68dd82c2afcd1f08b5ee632b540
Alexander <danilov@ralist.ru>
2022-08-19 14:56:50 +0300

    tech/add sentry (#19)
    * tech/add sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {{devIndicators: {autoPrerender: boolean}, reactStrictMode: boolean, swcMinify: boolean}} */
+
+const { withSentryConfig } = require('@sentry/nextjs');
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true,
+  authToken: 'a00b1bc1e8a94439b590eebd2d08b346f4c52bde1ed342e58f839dd2aae2566b'
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,4 +22,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
---
+++ sentry.client.config.js
@@ -0,0 +1,15 @@
+import * as Sentry from "@sentry/nextjs";
+import { BrowserTracing } from "@sentry/tracing";
+
+const SENTRY_DSN = process.env.FRONT_SENTRY_DSN || process.env.FRONT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || "https://9c58ffe772f04d56928df29b7dcc6303@o1367671.ingest.sentry.io/6668634",
+
+  // This enables automatic instrumentation (highly recommended), but is not
+  // necessary for purely manual usage
+  integrations: [new BrowserTracing()],
+
+  // To set a uniform sample rate
+  tracesSampleRate: 0.2
+});
