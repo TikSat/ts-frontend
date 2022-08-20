84d5475520dbb5e20b97bc21b68a02e8649d5252
Emperdeon <emperdeon@protonmail.com>
2022-08-20 17:31:12 +0300

    TS-19 Added releases and branch environment

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,6 +6,9 @@ const SENTRY_DSN = process.env.FRONT_SENTRY_DSN || process.env.FRONT_PUBLIC_SENT
 Sentry.init({
   dsn: SENTRY_DSN || "https://9c58ffe772f04d56928df29b7dcc6303@o1367671.ingest.sentry.io/6668634",
 
+  environment: process.env.RUN_ENV ?? "development",
+  release: process.env.SENTRY_RELEASE,
+
   // This enables automatic instrumentation (highly recommended), but is not
   // necessary for purely manual usage
   integrations: [new BrowserTracing()],
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,8 +8,12 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://9c58ffe772f04d56928df29b7dcc6303@o1367671.ingest.sentry.io/6668634',
+
+  environment: process.env.RUN_ENV ?? "development",
+  release: process.env.SENTRY_RELEASE,
+
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
