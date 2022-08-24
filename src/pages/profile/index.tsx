6d91c53f8ad79513c619610a7cdbf256fcca0bb4
ralist <danilov@ralist.ru>
2022-08-24 17:33:54 +0300

    feature/userStore

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,32 +1,13 @@
 import { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
-import { fetchApi } from '@core/helpers/api/fetcher';
-import { ApiRoutes } from '@core/routes';
-import { useActions } from '@core/hooks/useActions';
+import React from 'react';
 import { useTypedSelectors } from '@core/hooks/useTypedSelectors';
 import { Profile } from '@core/components/pages/Profile/Profile';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const ProfilePage: NextPage = () => {
-  const router = useRouter();
-  const { setUser } = useActions();
   const { user } = useTypedSelectors((state) => state.user);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (!user) {
-      fetchApi(ApiRoutes({}).profile, {
-        token: window.localStorage.getItem('token'),
-      }).then((res) => setUser(res));
-    }
-    setLoading(false);
-
-    if (!user && !loading) {
-      router.push('/sign_in');
-    }
-  }, [user]);
-
-  return <Profile {...user} />;
+  return user ? <Profile {...user} /> : <Link href={'/sign_in'}> Sign in </Link>;
 };
 
 export default ProfilePage;
